9a1ed041a0dbec1fdacd5bf2f429577c9e39bf9d
小峰 <dwfn3398@corp.netease.com>
2021-05-16 20:34:40 +0800

    Add feedback

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ const path = require('path')
 const http = require('http')
 
 const html = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8').toString()
+const feedback = fs.readFileSync(path.join(__dirname, 'feedback.html'), 'utf8').toString()
 
 http.createServer(function (req, res) {
   console.log(req.headers)
-  res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"})
-  res.write(html, 'utf-8')
+  if (req.url === '/receive') {
+    res.writeHead(200, {"Content-Type": "application/json; charset=utf-8"})
+    res.write('{}', 'utf-8')
+  } else if (req.url === '/feedback') {
+    res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"})
+    res.write(feedback, 'utf-8')
+  } else {
+    res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"})
+    res.write(html, 'utf-8')
+  }
   res.end()
 }).listen(5400)
